801d857128dea4e24d74a385bd0c32878a21ab24
Karol Wolski <poczta@karol-wolski.pl>
2020-03-05 21:26:36 +0100

    Add header and navigation (#1)
    Add focus trap for mobile version

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,44 @@
 import '../scss/style.scss';
+import createFocusTrap from 'focus-trap';
+
+const menuBtn = document.querySelector('#btn-menu');
+const navMenu = document.querySelector('#nav-menu');
+const navMenuLink = document.querySelectorAll('.menu__link');
+const menuIcon = menuBtn.firstChild;
+
+const focusTrapMobileNav = createFocusTrap('#navigation');
+
+const MobileNavigationClose = () => {
+  menuBtn.setAttribute('aria-expanded', 'false');
+  navMenu.classList.remove('isActive');
+  menuIcon.classList.remove('fa-times');
+  menuIcon.classList.add('fa-bars');
+  focusTrapMobileNav.deactivate();
+};
+
+const MobileNavigationOpen = () => {
+  menuBtn.setAttribute('aria-expanded', 'true');
+  navMenu.classList.add('isActive');
+  menuIcon.classList.remove('fa-bars');
+  menuIcon.classList.add('fa-times');
+  focusTrapMobileNav.activate();
+};
+
+const mobileNavigation = () => {
+  menuBtn.addEventListener('click', () => {
+    navMenuLink.forEach(item =>
+      item.addEventListener('click', () => MobileNavigationClose())
+    );
+
+    const menuToggle = navMenu.classList.contains('isActive')
+      ? MobileNavigationClose()
+      : MobileNavigationOpen();
+    return menuToggle;
+  });
+};
+
+const init = () => {
+  mobileNavigation();
+};
+
+init();
